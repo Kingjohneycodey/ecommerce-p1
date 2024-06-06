a4e53c2e87638edf588de70a61cebe74e101b80f
Brown <memoyebrown@gmail.com>
2024-06-06 13:56:29 +0100

    fix z-index for MainNavigationBar

--- src/components/ui/header/main-navigation-bar.jsx
+++ src/components/ui/header/main-navigation-bar.jsx
@@ -27,7 +27,7 @@ export function MainNavigationBar() {
 
   return (
     <>
-      <div className="relative flex max-h-14 min-h-12 items-center justify-between gap-4 bg-background px-8 py-4 max-lg:overflow-hidden lg:px-16">
+      <div className="relative z-40 flex max-h-14 min-h-12 items-center justify-between gap-4 bg-background px-8 py-4 max-lg:overflow-hidden lg:px-16">
         <div className="flex gap-2">
           <Button
             size="icon"
