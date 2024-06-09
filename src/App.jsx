a58c55b51fbd9c9d8ce55ec87a55ac61e99b77a5
oskiconcept <oscarjoseph7991@gmail.com>
2024-06-09 13:17:19 +0100

    more file changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { RouterProvider } from 'react-router-dom'
 import routes from './router'
-import Slids from './components/Slids';
 
 function App() {
   // we can wrap all other providers here I guess
-  return <>
-  <RouterProvider router={routes} />
-  <Slids/>
-  </>
+  return (
+    <>
+      <RouterProvider router={routes} />
+    </>
+  )
 }
 
-export default App;
- 
+export default App
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,12 @@
+import Slids from '@/components/Slids'
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      Home
+      <Slids />
+    </div>
+  )
 }
 
 export default Home
